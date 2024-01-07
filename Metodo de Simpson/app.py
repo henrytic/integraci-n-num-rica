3f0cde4a56dbe90c94dda68dbc6e3d0389bf14a5
Henry <108030609+henrytic@users.noreply.github.com>
2024-01-06 19:35:59 -0500

    Update app.py

--- Metodo de Simpson/app.py
+++ Metodo de Simpson/app.py
@@ -62,5 +62,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-#    app.run(debug=True)
+ #   app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
